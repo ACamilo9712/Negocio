4b4eff0dcb4616d6a18de8e14d3aaba9981acd85
andres.hoyos <andres.hoyos@ias.com.co>
2020-04-23 18:21:48 -0500

    Puesta en marcha con base de datos en contenedor con imagen postgres

--- src/main/java/com/example/Negocio/Configuration/DataSourceConfiguration.java
+++ src/main/java/com/example/Negocio/Configuration/DataSourceConfiguration.java
@@ -1,5 +1,8 @@
 package com.example.Negocio.Configuration;
 
+import com.example.Negocio.Configuration.domain.DataBaseCredentials;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -13,12 +16,25 @@ import javax.sql.DataSource;
 public class DataSourceConfiguration {
 
     @Bean
-    @Profile({"test", "dev"})
+    @Profile({"test"})
     public DataSource dataSource(){
         return new EmbeddedDatabaseBuilder()
                 .setType(EmbeddedDatabaseType.H2)
                 .addScript("classpath:scripts/schema.sql")
               //  .addScript("classpath:scripts/data.sql")
                 .build();
+    }
+
+    @Bean
+    @Profile({"dev"})
+    public DataSource hikariDatasource(DataBaseCredentials credentials) {
+        HikariConfig config = new HikariConfig();
+        config.setDataSourceClassName("org.postgresql.ds.PGSimpleDataSource");
+        config.addDataSourceProperty("databaseName", credentials.getDatabase());
+        config.addDataSourceProperty("portNumber", credentials.getPort());
+        config.addDataSourceProperty("serverName", credentials.getHost());
+        config.setUsername(credentials.getUsername());
+        config.setPassword(credentials.getPassword());
+        return new HikariDataSource(config);
     }
 }
