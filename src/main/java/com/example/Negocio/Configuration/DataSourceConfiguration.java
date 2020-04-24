7734e6a73d3424bc35be93ddcf38aec82b38b24c
andres.hoyos <andres.hoyos@ias.com.co>
2020-04-24 14:57:08 -0500

    changes into back

--- src/main/java/com/example/Negocio/Configuration/DataSourceConfiguration.java
+++ src/main/java/com/example/Negocio/Configuration/DataSourceConfiguration.java
@@ -6,7 +6,6 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
