adcedefb3f23a87363b4a2cab9ec8774471afb2b
andres.hoyos <andres.hoyos@ias.com.co>
2020-05-13 09:07:52 -0500

    CORS actualization

---
+++ Variables/src/main.java
@@ -0,0 +1,20 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        // string, entero, booleano, float, double, long
+        String texto = "El mas grande de colombia A/N";
+
+        int numeroEntero = -22+ 88;
+
+
+        int resultado=9;
+
+        if(numeroEntero < 84 && numeroEntero ==6){
+            //Primer Filtro
+         resultado = numeroEntero*8;
+    }
+        System.out.println(resultado);
+
+    }
+}
--- src/main/java/com/example/Negocio/Controllers/ProductController.java
+++ src/main/java/com/example/Negocio/Controllers/ProductController.java
@@ -17,25 +17,25 @@ public class ProductController {
 
     private final ProductsService service;
 
-
+    @CrossOrigin
     @PostMapping
     public ProductOperationRequest insertOne(@RequestBody ProductOperationRequest operationRequest) {
         service.insertOne(operationRequest.getName(), operationRequest.getDescription(), operationRequest.getBasePrice(), operationRequest.getTaxRate(),
                 operationRequest.getProductStatus(), operationRequest.getInventoryQuantity());
         return operationRequest;
     }
-
+    @CrossOrigin
     @GetMapping("/{id}")
     public ProductOperation findById(@PathVariable Long id) {
         ProductOperation optionalUser = service.findById(id);
         return optionalUser;
     }
-
+    @CrossOrigin
     @GetMapping("list")
     public List<ProductOperationRequest> findAll() {
         return service.findAll();
     }
-
+    @CrossOrigin
     @PutMapping("/{id}")
     public ProductOperation updateOne(@PathVariable Long id, @RequestBody ProductOperationRequest operationRequest) {
         service.updateOne(id, operationRequest.getName(), operationRequest.getDescription(), operationRequest.getBasePrice(), operationRequest.getTaxRate(),
@@ -43,7 +43,7 @@ public class ProductController {
         return service.updateOne(id, operationRequest.getName(), operationRequest.getDescription(), operationRequest.getBasePrice(), operationRequest.getTaxRate(),
                 operationRequest.getProductStatus(), operationRequest.getInventoryQuantity());
     }
-
+    @CrossOrigin
     @DeleteMapping("/{id}")
     public ProductOperation deleteOne(@PathVariable Long id) {
         return service.deleteOne(id);
